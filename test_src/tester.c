9e337d3518487ac41e648bb7f9ebe9a7c68b9f4e
Hassan Nadeem <hnadeem@vt.edu>
2017-12-06 15:51:56 -0500

    fix mmap write test

--- test_src/tester.c
+++ test_src/tester.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 
 /* mmap */
 #include <fcntl.h>
@@ -8,7 +9,6 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-#define TMP_FILE_NAME "tester_tmp_test_file"
 #define TMP_FILE_SIZE 4000 // don't change for now
 
 // Debugging Macros
@@ -16,6 +16,33 @@
 #define DBGM(var, type, desc)   printf(desc" = %"#type"\n", var)
 #define PRINT(msg, ...)         printf(msg, ##__VA_ARGS__)
 #define ERROR(msg, ...)         printf("ERROR: "msg, ##__VA_ARGS__)
+
+int isValidChar(char c){
+    if(
+        (c > 'A' && c < 'Z') ||
+        (c > 'a' && c < 'z')
+    ){
+        return 1;
+    }
+    
+    return 0;
+}
+
+char *getRandomString(char *buffer, size_t size){
+    char c;
+    
+    for (size_t i = 0; i < size-1; i++){
+        do{
+            c = rand();
+        }while(!isValidChar(c));
+        
+        buffer[i] = c;
+    }
+    
+    buffer[size - 1] = 0;
+
+    return buffer;
+}
 
 unsigned char *getRandomBytes(unsigned char *buffer, size_t size){
     for (size_t i = 0; i < size; i++){
@@ -129,6 +156,7 @@ int test_mmapRead(char *mount_filename){
         }
     }
     
+    munmap(mmappedData, sbuf.st_size);
     close(fd);
     return 0;
 }
@@ -143,17 +171,43 @@ int test_mmapWrite(char *mount_filename){
     getRandomBytes(data, TMP_FILE_SIZE);
     
     /* MMAP Write the file */
-    /* Flags: Write Only | create new if file does not exist | truncate old file to zero  */
-    if( (fd = open(mount_filename, O_WRONLY | O_CREAT | O_TRUNC, (mode_t)0600)) == -1 ){
+    /* Flags: Read/Write Only | create new if file does not exist | truncate old file to zero  */
+    if( (fd = open(mount_filename, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600)) == -1 ){
         ERROR("open\n");
         return -1;
     }
     
-    if(write(fd, data, sizeof(data)) == -1){
+    /* lseek to stretch the file */
+    if (lseek(fd, TMP_FILE_SIZE-1, SEEK_SET) == -1){
+        close(fd);
+        ERROR("lseek");
+        return -1;
+    }
+    
+    /* Write something at the end of the file to change file size */
+    if(write(fd, "", 1) == -1){
         close(fd);
         ERROR("write\n");
         return -1;
     }
+    
+    unsigned char* mmappedData = mmap(0, TMP_FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(mmappedData == MAP_FAILED){
+        ERROR("mmap\n");
+        return -1;
+    }
+    
+    /* Write test data to mmap */
+    for (size_t i = 0; i < TMP_FILE_SIZE; i++){
+        mmappedData[i] = data[i];
+    }
+    
+    // Write it now to disk
+    if(msync(mmappedData, TMP_FILE_SIZE, MS_SYNC) == -1){
+        ERROR("Could not sync the file to disk");
+    }
+    
+    /*-----------------------------------------------*/
     
     /* Read data without mmap */
     mount_fptr = fopen(mount_filename, "rb");
@@ -170,17 +224,23 @@ int test_mmapWrite(char *mount_filename){
             DBG(i, lu);
             DBG(data[i], u);
             DBG(data_read[i], u);
+            DBG(mmappedData[i], u);
             ERROR("MMAP data not equal\n");
             return -1;
         }
     }
     
+    munmap(mmappedData, TMP_FILE_SIZE);
     close(fd);
     return 0;
 }
 
 int main(int argc, char **argv){
     setbuf(stdout, NULL); // Disable stdout buffering
+    // seed the rand function
+    struct timeval time;
+    gettimeofday(&time,NULL);
+    srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
     
     printf("=====================================\n");
     
@@ -192,14 +252,15 @@ int main(int argc, char **argv){
     
     char src_filename[200];
     char mount_filename[200];
+    char tmp_file_name[20];
     
-    sprintf(src_filename, "%s/%s", argv[1], TMP_FILE_NAME);
-    sprintf(mount_filename, "%s/%s", argv[2], TMP_FILE_NAME);
+    getRandomString(tmp_file_name, sizeof(tmp_file_name));
+    
+    sprintf(src_filename, "%s/%s", argv[1], tmp_file_name);
+    sprintf(mount_filename, "%s/%s", argv[2], tmp_file_name);
     
     DBG(src_filename, s);
     DBG(mount_filename, s);
-    
-    srand((unsigned int) time (NULL)); // seed the rand function
     
     /* ========== Buffered IO Test ============= */
     PRINT("--------------------\n");
